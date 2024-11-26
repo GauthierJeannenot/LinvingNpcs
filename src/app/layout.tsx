b645bb6e528846dac6b94a466680f117e2ea31de
GauthierJeannenot <gauthier.jeannenot@teksial.com>
2024-11-26 12:01:14 +0100

    redirect in layout if user is not connected

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { AppProvider } from '@/lib/context/AppContext';
 import { LeftMenu } from '@/ui/LeftMenu';
 import { getServerSession } from 'next-auth/next';
 import { config } from '@/lib/auth';
+import { redirect } from 'next/navigation';
 
 export default async function RootLayout({
   children,
@@ -11,28 +12,21 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const session = await getServerSession(config);
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
   return (
     <html lang="en">
-      {session ? (
-        <AppProvider userId={session.user.id}>
-          <body className="bg-gray-900 text-gray-100">
-            <div className="flex h-screen">
-              <LeftMenu />
-              <div className="flex-grow h-full bg-gray-850 shadow-inner p-4 overflow-y-hidden">
-                {children}
-              </div>
-            </div>
-          </body>
-        </AppProvider>
-      ) : (
+      <AppProvider userId={session.user.id}>
         <body className="bg-gray-900 text-gray-100">
           <div className="flex h-screen">
+            <LeftMenu />
             <div className="flex-grow h-full bg-gray-850 shadow-inner p-4 overflow-y-hidden">
-              <a href="/api/auth/signin">Please Log in</a>
+              {children}
             </div>
           </div>
         </body>
-      )}
+      </AppProvider>
     </html>
   );
 }
